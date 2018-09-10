691dec0cb7248f4800ba0e8e4101fc3140de47ca
Remi Gascou <remi@gascou.net>
2018-09-10 11:36:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+##SDES implementation
+
+from lib import *
+
+
+print(SDES_encrypt("10101010", "0000000000"))
+print()
+#assert(SDES_encrypt("10101010", "0000000000") == "00010001")
+
+#print(SDES_encrypt("10101010", "1110001110"))
+#assert(SDES_encrypt("10101010", "1110001110") == "11001010")
+
+#print(SDES_encrypt("01010101", "1110001110"))
+#assert(SDES_encrypt("01010101", "1110001110") == "01110000")
+
+#print(SDES_encrypt("10101010", "1111111111"))
+#assert(SDES_encrypt("10101010", "1111111111") == "00000100")
